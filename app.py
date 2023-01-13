3f267c9e66e4dca9182f425e2b1ab75ce75be1c0
nicocarcamo <nicolascarcamonaccarino@gmail.com>
2023-01-13 15:40:34 +0000

    user routes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_cors import CORS
 from dotenv import load_dotenv
-from models import db, Contact
+from models import db, User
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -19,60 +19,63 @@ def root():
     return jsonify({ "message": "Welcome to my Flask API" }), 200
 
 
-@app.route('/api/contacts', methods=['GET'])
-def get_contacts():
+@app.route('/api/users', methods=['GET'])
+def get_users():
 
-    # SELECT * FROM contacts;
-    contacts = Contact.query.all() # [<Contact 1>, <Contact 2>]
-    contacts = list(map(lambda contact: contact.serialize(), contacts)) # convertir en diccionarios
+    # SELECT * FROM users;
+    users = User.query.all() # [<Contact 1>, <Contact 2>]
+    users = list(map(lambda user: user.serialize(), users)) # convertir en diccionarios
 
-    return jsonify(contacts), 200
+    return jsonify(users), 200
 
-@app.route('/api/contacts', methods=['POST'])
-def crear_contact():
-    # INSERT INTO contacts (name, email, phone) VALUES (?, ?, ?)
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    # INSERT INTO users (name, lastname, email, password) VALUES (?, ?, ?)
 
     datos = request.get_json()
-    contact = Contact()
-    contact.name = datos['name']
-    contact.email = datos['email']
-    contact.phone = datos['phone']
-    contact.save() # ejecuta los dos siguiente comandos internamente
+    user = User()
+    user.name = datos['name']
+    user.lastname = datos['lastname']
+    user.email = datos['email']
+    user.password = datos['password']
+    user.save() # ejecuta los dos siguiente comandos internamente
 
-    #db.session.add(contact)
+    #db.session.add(user)
     #db.session.commit()
 
-    return jsonify(contact.serialize()), 201
+    return jsonify(user.serialize()), 201
 
 
-@app.route('/api/contacts/<int:id>', methods=['PUT'])
-def actualizar_contact(id):
-    # UPDATE contacts SET name="", email="", phone="" WHERE id = ?
+@app.route('/api/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    # UPDATE user SET name="", lastname="" email="", password="" WHERE id = ?
     
     name = request.json.get('name') # None
+    lastname = request.json.get('lastname') # None
     email =  request.json.get('email') # None
-    phone =  request.json.get('phone') # None
+    password =  request.json.get('password') # None
 
-    # SELECT * FROM contacts WHERE id = ?
-    contact = Contact.query.get(id)
-    contact.name = name
-    contact.email = email
-    contact.phone = phone
-    contact.update()
+    # SELECT * FROM users WHERE id = ?
+    user = User.query.get(id)
+    user.name = name
+    user.lastname = lastname
+    user.email = email
+    user.password = password
+    user.update()
     
     #db.session.commit()
 
-    return jsonify(contact.serialize()), 200
+    return jsonify(user.serialize()), 200
 
-@app.route('/api/contacts/<int:id>', methods=['DELETE'])
-def eliminar_contact(id):
-    # SELECT * FROM contacts WHERE id = ?
-    contact = Contact.query.get(id)
+@app.route('/api/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    # SELECT * FROM users WHERE id = ?
+    user = User.query.get(id)
 
-    # DELETE FROM contacts WHERE id=?
-    contact.delete()
+    # DELETE FROM users WHERE id=?
+    user.delete()
 
-    return jsonify({ "message": "Contacto Eliminado" }), 200
+    return jsonify({ "message": "User Deleted" }), 200
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,19 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class Contact(db.Model):
-    __tablename__ = 'contacts'
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
+    lastname = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(100))
-    phone = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
+            "lastname": self.lastname,
             "email": self.email,
-            "phone": self.phone
+            "password": self.password
         }
 
     def save(self):
@@ -26,3 +28,79 @@ class Contact(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    climate = db.Column(db.String(200), nullable=False)
+    terrain = db.Column(db.String(200), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'climate': self.climate,
+            'terrain': self.terrain
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+# class Favorite(db.Model):
+#     __tablename__ = 'favorites'
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     people_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+#     planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+#     def serialize(self):
+#         return {
+#             'id': self.id,
+#             'user_id': self.user_id,
+#             'people_id': self.people_id,
+#             'planet_id': self.planet_id
+#         }
+
+#     def save(self):
+#         db.session.add(self)
+#         db.session.commit()
+
+#     def update(self):
+#         db.session.commit()
+
+#     def delete(self):
+#         db.session.delete(self)
+#         db.session.commit()
